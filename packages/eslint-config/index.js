218e944aa5e89c4acfdd9bd3efd69675227c8786
tszhong0411 <lainelson411@gmail.com>
2023-04-05 22:31:12 +0800

    fix(eslint-config): add jsx-a11y

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -10,6 +10,7 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
     'plugin:prettier/recommended',
   ],
   overrides: [],
